fbac9ac6904943a12369bdc1afd2d5c27c331bc3
BigVito7 <biggvito7@gmail.com>
2025-02-26 16:35:20 +0200

    Months amount of days

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,6 +1,20 @@
 package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
-    public void vowelDeterminer(char character) {
+    public void vowelDeterminer(char character)
+    {
+        char lowerChar = Character.toLowerCase(character);
+
+
+        if (lowerChar >= 'a' && lowerChar <= 'z') {
+
+            if (lowerChar == 'a' || lowerChar == 'e' || lowerChar == 'i' || lowerChar == 'o' || lowerChar == 'u') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,28 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+
+        switch (month) {
+            case 1:  // January
+            case 3:  // March
+            case 5:  // May
+            case 7:  // July
+            case 8:  // August
+            case 10: // October
+            case 12: // December
+                System.out.println(31); // Months with 31 days
+                break;
+            case 4:  // April
+            case 6:  // June
+            case 9:  // September
+            case 11: // November
+                System.out.println(30); // Months with 30 days
+                break;
+            case 2:  // February
+                System.out.println(28); // February has 28 days (assuming it's not a leap year)
+                break;
+            default:
+                System.out.println("wrong number!"); // Invalid month number
+        }
     }
 }
